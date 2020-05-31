4d28d102405243c8dd375e9de4c8138f5df11da0
Dawid Domaradzki <dawid-kun@wp.pl>
2020-05-31 19:08:37 +0200

    primal scheme

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,27 @@
-"use strict";
-
-// service worker registration - remove if you're not going to use it
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}
-
-// place your code below
-
 
 console.log(`Hello world!`);
 
+const list = document.querySelector('.repositories__list--js');
+
+
+fetch('https://api.github.com/users/dawiddomaradzki/repos')
+  .then(resp => resp.json())
+  .then(resp => {
+    const repos = resp;
+    for (const repo of repos) {
+      console.log(`${repo.name} ${repo.html_url}`);
+      const {description, html_url, name} = repo;
+      list.innerHTML += `
+      <li class="list__item">
+      <a class="list__link" href="${html_url}">${name}</a>
+      <p>${description ? description : "brak opisu"}</p>
+      </li>
+      `;
+    }
+
+  })
+
+  .catch(err => {
+    console.log(err);
+  })
 
