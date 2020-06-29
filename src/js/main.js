e15b690a3a189237fcf7d255d5b812c47e671870
Dawid Domaradzki <dawid-kun@wp.pl>
2020-06-29 11:05:40 +0200

    feat: logo & cover

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,38 @@
 
 console.log(`Hello world!`);
 
-const list = document.querySelector('.repositories__list--js');
+const list = document.querySelector('.projects-list--js');
 
 
-fetch('https://api.github.com/users/dawiddomaradzki/repos')
+fetch('https://api.github.com/users/dawiddomaradzki/repos?sort=updated')
   .then(resp => resp.json())
   .then(resp => {
     const repos = resp;
     for (const repo of repos) {
-      console.log(`${repo.name} ${repo.html_url}`);
-      const {description, html_url, name} = repo;
+      console.log(repo);
+      const { description, homepage, html_url, name } = repo;
       list.innerHTML += `
-      <li class="list__item">
-      <a class="list__link" href="${html_url}">${name}</a>
-      <p>${description ? description : "brak opisu"}</p>
-      </li>
+
+       <li class="project">
+            <div class="project__container">
+              <img src="assets/img/githubpurple.svg" alt="github photo">
+              <h3 class="project__title">${name}</h3>
+              ${
+                description ? `<p class="project__description">${description}</p>` : 'No description'
+              }
+              
+            </div>
+            <div class="project__footer">
+
+            ${
+        homepage ? `<a class="project__link project__link--demo" href="${homepage}" target="_blank" rel="nofollow noreferrer" title="Demo: ${name}">Demo</a>` : ''
+        }
+              
+              <a class="project__link project__link--source" href="${html_url}" target="_blank" rel="nofollow noreferrer"  title="Source code: ${name}">Github</a>
+            </div>
+          </li>
       `;
     }
-
   })
 
   .catch(err => {
